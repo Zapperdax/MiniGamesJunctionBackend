580bedb865f36fdff7acdcb4999843ef7204e356
Zapperdax <qa80986@gmail.com>
2023-01-10 16:11:01 +0500

    Backend

--- src/index.js
+++ src/index.js
@@ -5,13 +5,29 @@ require("./db/mongoose");
 const userRoute = require("./routes/userRoute");
 
 const app = express();
-
+const http = require("http");
+const { Server } = require("socket.io");
 const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
 app.use(userRoute);
 
-app.listen(port, () => {
-  console.log("listening on port" + port);
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+  socket.on("sendMessage", ({ sendMessage }) => {
+    socket.broadcast.emit("receiveMessage", { sendMessage });
+  });
+});
+
+server.listen(port, () => {
+  console.log("listening on port " + port);
 });
